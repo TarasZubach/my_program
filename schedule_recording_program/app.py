34dc395653421f24f57fe75ec6983bf6a83b174e
TarasZubach <richardpy75@gmail.com>
2019-12-10 20:07:28 +0200

    Refactoring schedule_recording_program

--- schedule_recording_program/app.py
+++ schedule_recording_program/app.py
@@ -1,55 +1,71 @@
+import io, os
 from datetime import date, datetime
-import os
-
+import json
 def scheduled_date_file_recording():
     task_name = input("Введите задание: ")
-    day_month_original = input("Введите планируемое время в виде день.месяц:")
+    day_month_original = input("Введите планируемое время в виде день.месяц(просьба прописать через точку):")
 
     # Do I convert keyboard input to a file with the extension txt
-    day_month_conversion_to_txt = f"{task_name}.txt"
+    task_name_conversion_to_json = f"{task_name}.json"
 
-    convert_to_num = day_month_original.split(".")
+    # I broke the date and month into separate elements so that you can work with them
+    day_month_original_conversion_to_num = day_month_original.split('.')
 
-    if day_month_conversion_to_txt in os.listdir("../st_app"):
+    count_days_and_month = 0
+
+    for day_and_month in day_month_original_conversion_to_num:
+        try:
+            day_and_month = int(day_and_month)
+        except ValueError:
+            pass
+        if isinstance(day_and_month, int):
+            count_days_and_month += 1
+
+    if task_name_conversion_to_json in os.listdir("../schedule_recording_program"):
         print("This name of file are used")
 
-    for file_name in convert_to_num:
-        if file_name.isdigit():
+    if count_days_and_month > 0:
             # I check if there is such a file in the directory
-            if day_month_conversion_to_txt not in os.listdir("../st_app"):
-                # Here I take only the data I need, that is, the date and month, remove the rest with a slice, and then
-                # replace the hyphen with a period.
-                day_month_second_version = str(datetime.strptime(day_month_original, "%d.%m"))[5:10].replace("-", ".")
+        if task_name_conversion_to_json not in os.listdir("../schedule_recording_program"):
+            # Here I take only the data I need, that is, the date and month, remove the rest with a slice, and then
+            # replace the hyphen with a period.
+            day_month_second_version = str(datetime.strptime(day_month_original, "%d.%m"))[5:10].replace("-", ".")
 
-                # I’m doing practically the same thing here
+            # I’m doing practically the same thing here
+            today = str(datetime.strptime(f"{date.today().month}.{date.today().day}", "%d.%m"))[5:10].replace(
+            "-", ".")
+
+            with io.open(task_name_conversion_to_json, "w", encoding='utf8') as file:
+                #file.write(f"Scheduled from {today} to {day_month_second_version}")
+                file.write(json.dumps({task_name: f"Scheduled from {today} to {day_month_second_version}"}, sort_keys=True,
+                           separators=(",", ": "), indent=4, ensure_ascii=False))
+
+
+    elif count_days_and_month == 0:
+        if day_month_original == "завтра":
+            if task_name_conversion_to_json not in os.listdir("../schedule_recording_program"):
+
                 today = str(datetime.strptime(f"{date.today().month}.{date.today().day}", "%d.%m"))[5:10].replace(
                 "-", ".")
 
-                with open(day_month_conversion_to_txt, "w") as file:
-                    file.write(f"Scheduled from {today} to {day_month_second_version}")
+                with io.open(task_name_conversion_to_json, "w", encoding="utf8") as file:
+                # Here I do almost the same thing as above, but add + 1 day, based on the fact that i need the next
+                # day in the task, that is, tomorrow.
+                    file.write(json.dumps({task_name : f"Scheduled on {today.replace(today[1:2], str(int(today[1:2]) + 1))}"},
+                    sort_keys=True, separators=(",", ": "), indent=4, ensure_ascii=False))
 
 
-        elif file_name.isalpha():
-            if file_name == "завтра":
-                if day_month_conversion_to_txt not in os.listdir("../st_app"):
+        elif task_name == "послезавтра":
 
-                    today = str(datetime.strptime(f"{date.today().month}.{date.today().day}", "%d.%m"))[5:10].replace(
-                    "-", ".")
+            if task_name_conversion_to_json not in os.listdir("../schedule_recording_program"):
 
-                    with open(day_month_conversion_to_txt, 'w') as file:
-                    # Here I do almost the same thing as above, but add + 1 day, based on the fact that i need the next
-                    # day in the task, that is, tomorrow.
-                        file.write(f"Scheduled on {today.replace(today[1:2], str(int(today[1:2]) + 1))}")
+                today = str(datetime.strptime(f"{date.today().month}.{date.today().day}", "%d.%m"))[5:10].replace(
+                "-", ".")
 
-            elif file_name == "послезавтра":
-                if day_month_conversion_to_txt not in os.listdir("../st_app"):
-
-                    today = str(datetime.strptime(f"{date.today().month}.{date.today().day}", "%d.%m"))[5:10].replace(
-                    "-", ".")
-
-                    with open(day_month_conversion_to_txt, "w") as file:
-                    # Then I do almost the same thing as above, but add + 2 day, based on the fact that I need the next day
-                    #  in the task, that is, the day after tomorrow.
-                        file.write(f"Scheduled on {today.replace(today[1:2], str(int(today[1:2]) + 2))}")
+                with io.open(task_name_conversion_to_json, "w", encoding="utf8") as file:
+                # Then I do almost the same thing as above, but add + 2 day, based on the fact that I need the next day
+                #  in the task, that is, the day after tomorrow.
+                    file.write(json.dumps({task_name : f"Scheduled on {today.replace(today[1:2], str(int(today[1:2]) + 2))}"},
+                    sort_keys = True, separators=(",", ": "), indent=4, ensure_ascii=False))
 
 print(scheduled_date_file_recording())

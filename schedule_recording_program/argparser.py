5ad11315e65d8ccf389eed4050a5a0c5eec61d39
TarasZubach <richardpy75@gmail.com>
2019-12-13 16:41:53 +0200

    Refactoring schedule_recording_program

---
+++ schedule_recording_program/argparser.py
@@ -0,0 +1,6 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-task", "--task_name", nargs=1, help="Введите задание: ")
+parser.add_argument("-day_and_month", "--day_and_month", nargs=1, help="Введите дату и месяц черезе точку: ")
+args = parser.parse_args()

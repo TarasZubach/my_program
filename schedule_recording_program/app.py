8c8257aa26661c5a37488319e2936e7d1e71ee3d
TarasZubach <richardpy75@gmail.com>
2019-12-14 18:05:49 +0200

    Refactoring schedule_recording_program

--- schedule_recording_program/app.py
+++ schedule_recording_program/app.py
@@ -3,42 +3,40 @@ import os
 import argparser
 from datetime import datetime, timedelta
 
-
-def schedule_recording_app():
-    imported_args = argparser.func_argparser()
-
-
-    task_name_convert_to_json = f"{''.join(imported_args.task_name)}.json"
-    split_day_and_month = "".join(imported_args.day_and_month)
-
+def write_json_file (func_arg):
+    imported_function_argument = argparser.func_argparser()
+    task_name_convert_to_json = f"{''.join(imported_function_argument.task_name)}.json"
 
     if task_name_convert_to_json in os.listdir("../schedule_recording_program"):
         print(f"{task_name_convert_to_json}: already exists")
     else:
+        with open(task_name_convert_to_json, "w") as file:
+            json.dump({''.join(imported_function_argument.task_name): f"Scheduled to {func_arg}"}, file,
+            sort_keys=True, indent=4, ensure_ascii=False)
 
-        try:
-            convert_day_and_month = datetime.strptime(split_day_and_month, "%d.%m").strftime("%d.%m")
 
-        except ValueError:
+def schedule_recording_app():
+    imported_args = argparser.func_argparser()
+    split_day_and_month = "".join(imported_args.day_and_month)
 
-            if split_day_and_month == "tomorrow":
-                scheduled_to_tomorrow = (datetime.today() + timedelta(days=1)).strftime("%d.%m")
+    try:
+        convert_day_and_month = datetime.strptime(split_day_and_month, "%d.%m").strftime("%d.%m")
 
-                with open(task_name_convert_to_json, "w") as file:
-                    json.dump({''.join(imported_args.task_name): f"Scheduled to {scheduled_to_tomorrow}"}, file,
-                        sort_keys=True, separators=(",", ": "), indent=4, ensure_ascii=False)
+    except ValueError:
 
-            elif split_day_and_month == "day_after_tomorrow":
-                scheduled_to_day_after_tomorrow = (datetime.today() + timedelta(days=2)).strftime("%d.%m")
+        if split_day_and_month == "tomorrow":
+            scheduled_to_tomorrow = (datetime.today() + timedelta(days=1)).strftime("%d.%m")
 
-                with open(task_name_convert_to_json, "w") as file:
-                    json.dump({''.join(imported_args.task_name): f"Scheduled to {scheduled_to_day_after_tomorrow}"},
-                        file, sort_keys=True, separators=(",", ": "), indent=4, ensure_ascii=False)
+            write_json_file(scheduled_to_tomorrow)
 
-        else:
-            with open(task_name_convert_to_json, "w") as file:
-                json.dump({''.join(imported_args.task_name): f"Scheduled to {convert_day_and_month}"},
-                    file, sort_keys=True, separators=(",", ": "), indent=4, ensure_ascii=False)
+        elif split_day_and_month == "day_after_tomorrow":
+            scheduled_to_day_after_tomorrow = (datetime.today() + timedelta(days=2)).strftime("%d.%m")
+
+            write_json_file(scheduled_to_day_after_tomorrow)
+
+    else:
+
+        write_json_file(convert_day_and_month)
 
 if __name__ == "__main__":
     schedule_recording_app()
